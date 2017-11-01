8e40ab04768192598d3c2487e1db9906f537b73e
Spencer Campbell <spencer@ultroneous.org>
2017-11-01 15:43:01 +0000

    Remove attempt at AJAX

--- source/_includes/transaction-ajax.js
+++
@@ -1,7 +0,0 @@
-// wait for the DOM to be loaded
-$(function() {
-// bind 'myForm' and provide a simple callback function
-$('#transaction-form').ajaxForm(function() {
-   alert("Transaction recorded.");
-});
-});

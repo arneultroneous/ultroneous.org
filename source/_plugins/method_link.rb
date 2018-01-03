1156e647c6823d819f448ec6298a8e48f476b5c8
Spencer Campbell <spencer.campbell@tamera.org>
2018-01-03 06:09:57 +0000

    Stop linking to unwritten prerequisites

--- source/_plugins/method_link.rb
+++ source/_plugins/method_link.rb
@@ -1,7 +1,17 @@
 module Jekyll
   module MethodLinkFilter
+    require 'find'
     def method_link(title)
-      "<a href=\"/guide/#{Utils.slugify(title)}\">#{title}</a>"
+      # Find out which method pages exist
+      page_titles = Find.find("source/guide").map { |path|
+        path[/\/([^\/]*)\/index.md/, 1] if path =~ /index.md$/
+      }.compact
+
+      # Return a link to the ones that exist
+      return "<a href=\"/guide/#{Utils.slugify(title)}\">#{title}</a>" if page_titles.include?(Utils.slugify(title))
+
+      # Return just the title of those that don't exist
+      return "#{title}"
     end
   end
 end
